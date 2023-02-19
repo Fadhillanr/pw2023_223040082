e48452301ea6bd2228429375f71c58e04e47ef74
Fadhillanr <fadhilla.223040082@mail.unpas.ac.id>
2023-02-19 17:45:54 +0700

    mengumpulkan tugas php

---
+++ tugas/tugas1/1c.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1c</title>
+
+    <style>
+        .kotakpertama {
+            text-align: center;
+            line-height: 50px;
+        }
+        .kotakkedua {
+            text-align: center;
+            line-height: 50px;
+            display: flex;
+        }
+        .kotakketiga {
+            text-align: center;
+            line-height: 50px;
+            display: flex;
+        }
+        .kotakpertama-1 {
+            width: 50px;
+            height: 50px;
+            background-color: pink;
+            border: 2px solid black;
+        }
+        .kotakkedua-1 {
+            width: 50px;
+            height: 50px;
+            background-color: pink;
+            border: 2px solid black;
+        }
+        .kotakkedua-2 {
+            width: 50px;
+            height: 50px;
+            background-color: pink;
+            border: 2px solid black;
+        }
+        .kotakketiga-1 {
+            width: 50px;
+            height: 50px;
+            background-color: pink;
+            border: 2px solid black;
+        }
+        .kotakketiga-2 {
+            width: 50px;
+            height: 50px;
+            background-color: pink;
+            border: 2px solid black;
+        }
+        .kotakketiga-3 {
+            width: 50px;
+            height: 50px;
+            background-color: pink;
+            border: 2px solid black;
+        }
+    </style>
+
+</head>
+<body>
+    <div class="kotakpertama">
+        <div class="kotakpertama-1">1</div>
+    </div>
+    <div class="kotakkedua">
+        <div class="kotakkedua-1">2</div>
+        <div class="kotakkedua-1">2</div>
+    </div>
+    <div class="kotakketiga">
+        <div class="kotakketiga-1">3</div>
+        <div class="kotakketiga-2">3</div>
+        <div class="kotakketiga-3">3</div>
+    </div>
+</body>
+</html>
