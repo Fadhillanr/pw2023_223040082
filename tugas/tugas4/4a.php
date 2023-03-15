6eb8ed1ffe1917ae65586ccbaf0071004558f20b
Fadhillanr <121225974+Fadhillanr@users.noreply.github.com>
2023-03-15 19:07:26 +0700

    mengumpulkan tugas php

---
+++ tugas/tugas4/4a.php
@@ -0,0 +1,7 @@
+<?php  
+//string element
+$array = ['epan', 'cipta', 'akan', 'u', 'asa', 'esok'];
+
+//saquence of numbers from 0
+echo "M$array[4] d$array[0] di$array[1]kan oleh apa yang kau kerj$array[2] hari ini, b$array[3]kan b$array[5]";
+?>
